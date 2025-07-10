cdebec44c52b474f83442f3ff3969a807918eb4d
Razeem Ahmad <razeem.a@applab.qa>
2025-07-10 12:13:56 +0300

    Updated with some Server level security fixes and dev robots.txt

--- dist/Docker/scripts/entrypoint.sh
+++ dist/Docker/scripts/entrypoint.sh
@@ -6,5 +6,10 @@ printenv | awk -F= '{ print "export " $1 "=\"" substr($0, index($0,$2)) "\"" }' 
 # Fix permissions if needed
 chmod 0644 /etc/container_env.sh
 
+# Copy robots.txt to web folder if DRUPAL_NGINX_ENV=dev
+if [ "$DRUPAL_NGINX_ENV" = "dev" ]; then
+  cp /app/Docker/app/dev.robots.txt /app/web/robots.txt
+fi
+
 # Start supervisord
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
